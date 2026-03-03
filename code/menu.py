ffd66132b73bb473f27cbc360fbebf301d5c80f7
Geane Alves <geanedarc2@gmail.com>
2026-03-03 18:34:05 -0300

    creating main loop connecting Menu and Game classes

--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from _curses import window
+
 
 class Menu:
     def __init__(self):
-        self.window = None
+        self.window = window
 
     def run(self, ):
         pass
